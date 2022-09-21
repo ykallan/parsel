70983e22342375efbb48d94c7b2e0ed94b55bede
ykallan <815583442@qq.com>
2022-09-21 16:21:32 +0800

    upload

--- parsel.go
+++ parsel.go
@@ -3,8 +3,8 @@ package parsel
 import (
 	"fmt"
 	"github.com/antchfx/htmlquery"
-	_ "github.com/antchfx/htmlquery"
 	"golang.org/x/net/html"
+	"regexp"
 	"strings"
 )
 
@@ -14,9 +14,10 @@ type Selector struct {
 	Text string
 	err  []error
 
+	linkFlag          bool
 	XpathNode         *html.Node
 	XpathStringResult []string
-	XpathNodeResult   []html.Node
+	XpathNodeResult   []*html.Node
 }
 
 func (s *Selector) Help() {
@@ -33,6 +34,63 @@ func (s *Selector) Load(text string) {
 	s.Text = text
 	s.XpathNode, err = htmlquery.Parse(strings.NewReader(text))
 	if err != nil {
-		panic(err.Error())
+		//panic(err.Error())
+		fmt.Println("load text catch err = ", err.Error())
 	}
+}
+
+func (s *Selector) Xpath(xpath string) *Selector {
+	fmt.Println("s.linkFlag =", s.linkFlag)
+	if s.linkFlag {
+		// already get A xpath node, can use link extract
+		newXpathNode := htmlquery.Find(s.XpathNode, xpath)
+		if len(newXpathNode) > 0 {
+			s.XpathNode = newXpathNode[0]
+			return s
+		} else {
+			s.XpathNode = nil
+			s.linkFlag = false
+		}
+	}
+
+	s.XpathNodeResult = htmlquery.Find(s.XpathNode, xpath)
+	// if got xpath node more than one, save the first one
+	if len(s.XpathNodeResult) > 0 {
+		s.XpathNode = s.XpathNodeResult[0]
+		s.linkFlag = true
+
+		return s
+	}
+	return s
+}
+
+func (s *Selector) Re(reString string) *Selector {
+	s.XpathStringResult = make([]string, 0)
+	compiled := regexp.MustCompile(reString)
+	match := compiled.FindAllStringSubmatch(s.Text, -1)
+	//fmt.Println(match)
+	for _, first := range match {
+		for _, second := range first {
+			s.XpathStringResult = append(s.XpathStringResult, second)
+		}
+	}
+	return s
+}
+
+func (s *Selector) Get() string {
+	return s.XpathNode.Data
+}
+
+func (s *Selector) GetAll() []string {
+	newStringSlice := make([]string, 0)
+	for _, node := range s.XpathNodeResult {
+		//fmt.Println(node.Data)
+		newStringSlice = append(newStringSlice, node.Data)
+	}
+
+	return newStringSlice
+}
+
+func (s *Selector) GetText() string {
+	return s.Text
 }
